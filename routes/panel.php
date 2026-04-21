68f79984f9149be38e80fcb46b060e5928829873
Melikşah Birgül <meliksahbrgl@gmail.com>
2026-04-22 00:47:46 +0300

    feat: auth endpoints added

--- routes/panel.php
+++ routes/panel.php
@@ -5,8 +5,17 @@ use Source\Pages\Presentation\Http\Controllers\ChangePageDetailsController;
 use Source\Pages\Presentation\Http\Controllers\CreatePageController;
 use Source\Pages\Presentation\Http\Controllers\ListPagesController;
 use Source\Pages\Presentation\Http\Controllers\PageDetailsController;
+use Source\Pages\Presentation\Http\Controllers\Users\LoginController;
 
 Route::get('pages', ListPagesController::class);
 Route::post('pages', CreatePageController::class);
 Route::patch('pages/{pageId}', ChangePageDetailsController::class);
 Route::get('pages/{pageId}', PageDetailsController::class);
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('login', LoginController::class);
+        Route::get('me');
+        Route::post('logout');
+    });
+});
