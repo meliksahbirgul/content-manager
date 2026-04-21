8ab3f3680077e2f072820b873e54136aeab9e472
Melikşah Birgül <meliksahbrgl@gmail.com>
2026-04-21 22:44:45 +0300

    fix: provider changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Source\Pages\Domain\Repository\Repository;
 use Source\Pages\Infrastructure\Persistence\PageRepository;
-use Source\Pages\Infrastructure\Persistence\PageRepositroy;
 
 class AppServiceProvider extends ServiceProvider
 {
