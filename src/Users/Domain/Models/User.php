e293774672fc18b9f21a9c93c93bbcfa6f2cf89b
Melikşah Birgül <meliksahbrgl@gmail.com>
2026-04-22 01:17:55 +0300

    fix: get user token

--- src/Users/Domain/Models/User.php
+++ src/Users/Domain/Models/User.php
@@ -5,11 +5,13 @@ namespace Source\Users\Domain\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
+    use HasApiTokens;
     /**
      * Get the attributes that should be cast.
      *
--- src/Users/Infrastructure/Persistence/UserRepository.php
+++ src/Users/Infrastructure/Persistence/UserRepository.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Source\Users\Infrastructure\Persistence;
 
+use DateTimeImmutable;
 use Source\Users\Domain\Entity\UserEntity;
 use Source\Users\Domain\Entity\UserTokenEntity;
 use Source\Users\Domain\Models\User as EloquentUser;
 use Source\Users\Domain\Repository\Repository;
+
+use function config;
 
 class UserRepository implements Repository
 {
@@ -23,10 +26,33 @@ class UserRepository implements Repository
 
     public function createTokenForUser(string $email): UserTokenEntity|null
     {
+        $user = EloquentUser::where('email', $email)->first();
+        if (! $user) {
+            return null;
+        }
+
+        $now = new DateTimeImmutable();
+
+        $accessSeconds   = config('sanctum.expiration');
+        $accessExpiresAt = $now->modify("+{$accessSeconds} seconds");
+        $accessToken     = $user->createToken(
+            name: 'access-token',
+            abilities: ['access-panel'],
+            expiresAt: $accessExpiresAt
+        );
+
+        $refreshSeconds  = config('sanctum.rt_expiration');
+        $refreshExpireAt = $now->modify("+{$refreshSeconds} seconds");
+        $refreshToken    = $user->createToken(
+            name: 'refresh-token',
+            abilities: ['issue-access-token'],
+            expiresAt: $refreshExpireAt,
+        );
+
         return new UserTokenEntity(
-            accessToken: '',
-            refreshToken: '',
-            expiresAt: 60,
+            accessToken: $accessToken->plainTextToken,
+            refreshToken: $refreshToken->plainTextToken,
+            expiresAt: (int) $accessSeconds,
         );
     }
 
