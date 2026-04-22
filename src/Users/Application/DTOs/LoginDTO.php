286682ead4dc92adba9e538e1002f1f0bc5bd9fc
Melikşah Birgül <meliksahbrgl@gmail.com>
2026-04-22 23:59:12 +0300

    fix: parameter missing control for login dto

--- src/Users/Application/DTOs/LoginDTO.php
+++ src/Users/Application/DTOs/LoginDTO.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Source\Users\Application\DTOs;
 
+use InvalidArgumentException;
+
+use function array_key_exists;
+
 readonly class LoginDTO
 {
     public function __construct(private string $email, private string $password) {}
@@ -11,6 +15,10 @@ readonly class LoginDTO
     /** @param array<string, mixed> $data */
     public static function fromRequest(array $data): self
     {
+        if (! array_key_exists('email', $data) || ! array_key_exists('password', $data)) {
+            throw new InvalidArgumentException('Parameters are missing.');
+        }
+
         return new self(
             email: $data['email'],
             password: $data['password'],
