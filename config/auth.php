6d057537326223c094d105c2e2927a6ee05cd350
Melikşah Birgül <meliksahbrgl@gmail.com>
2026-05-20 01:31:32 +0300

    conf: auth configuration changes

--- config/auth.php
+++ config/auth.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\User;
+use Source\Users\Domain\Models\User as EloquentUser;
 
 return [
 
@@ -64,7 +64,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', User::class),
+            'model' => env('AUTH_MODEL', EloquentUser::class),
         ],
 
         // 'users' => [
