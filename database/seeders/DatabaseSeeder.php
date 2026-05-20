f319fc01ad21e02bfad9dc3eec128daf284f6a94
Melikşah Birgül <meliksahbrgl@gmail.com>
2026-05-20 14:33:42 +0300

    fix: users table changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,5 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'uuid' => Uuid::uuid7()->toString(),
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    public function run(): void {}
 }
