75c5ec7e6edceb729893103d5fc3ef52561a7a14
Melikşah Birgül <meliksahbrgl@gmail.com>
2026-05-23 00:55:03 +0300

    feat: languages table and data added

---
+++ database/migrations/2026_05_22_215019_add_new_languages.php
@@ -0,0 +1,57 @@
+<?php
+
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Uuid;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $languages = [
+            [
+                'name' => 'English',
+                'code' => 'en',
+                'status' => 'active',
+            ],
+            [
+                'name' => 'Türkçe',
+                'code' => 'tr',
+                'status' => 'active',
+            ],
+            [
+                'name' => 'Deutsch',
+                'code' => 'de',
+                'status' => 'passive',
+            ]
+        ];
+
+        foreach ($languages as $language) {
+            DB::table('languages')
+                ->insert(
+                    [
+                        'uuid' => Uuid::uuid7()->toString(),
+                        'name' => $language['name'],
+                        'code' => $language['code'],
+                        'status' => $language['status'],
+                        'created_at' => CarbonImmutable::now('UTC')->format('Y-m-d H:i:s'),
+                        'updated_at' => CarbonImmutable::now('UTC')->format('Y-m-d H:i:s'),
+                    ],
+                );
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('languages')->delete();
+    }
+};
